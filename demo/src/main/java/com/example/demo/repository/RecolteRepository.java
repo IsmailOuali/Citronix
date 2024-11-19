927618f6bf78ed9990f0c020f61964c4d0985dae
unknown <ismail.ouali@ump.ac.ma>
2024-11-19 15:13:52 +0100

    Adding the Recolte repository

---
+++ demo/src/main/java/com/example/demo/repository/RecolteRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Recolte;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecolteRepository extends JpaRepository<Recolte, Long> {
+}
