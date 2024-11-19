be8d47fcabf023c7f701bc3e5161aa8d12784f91
unknown <ismail.ouali@ump.ac.ma>
2024-11-19 15:58:53 +0100

    Adding the champ service file

---
+++ demo/src/main/java/com/example/demo/service/impl/champServiceImpl.java
@@ -0,0 +1,6 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.service.champService;
+
+public class champServiceImpl implements champService {
+}
---
+++ demo/src/main/java/com/example/demo/service/impl/fermeServiceImpl.java
@@ -0,0 +1,6 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.service.fermeService;
+
+public class fermeServiceImpl implements fermeService {
+}
