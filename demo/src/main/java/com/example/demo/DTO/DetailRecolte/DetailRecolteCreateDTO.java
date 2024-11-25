426ee7809b2f5c9e1ded71733735496b77ccb459
unknown <ismail.ouali@ump.ac.ma>
2024-11-25 11:37:30 +0100

    CIT-26 Adding and displaying detailRecolte

--- demo/src/main/java/com/example/demo/DTO/DetailRecolte/DetailRecolteCreateDTO.java
+++ demo/src/main/java/com/example/demo/DTO/DetailRecolte/DetailRecolteCreateDTO.java
@@ -2,6 +2,7 @@ package com.example.demo.DTO.DetailRecolte;
 
 import com.example.demo.model.Arbre;
 import com.example.demo.model.Recolte;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,9 +15,9 @@ public class DetailRecolteCreateDTO {
 
     private double quantiteRecoltee;
 
-
+    @JsonProperty("arbre")
     private Arbre arbre;
 
-
+    @JsonProperty("recolte")
     private Recolte recolte;
 }
--- demo/src/main/java/com/example/demo/service/impl/DetailRecolteServiceImpl.java
+++ demo/src/main/java/com/example/demo/service/impl/DetailRecolteServiceImpl.java
@@ -6,9 +6,14 @@ import com.example.demo.DTO.DetailRecolte.DetailRecolteResponseDTO;
 import com.example.demo.mapper.DetailRecolteMapper;
 import com.example.demo.model.Arbre;
 import com.example.demo.model.DetailRecolte;
+import com.example.demo.model.DetailRecolteId;
+import com.example.demo.model.Recolte;
 import com.example.demo.repository.ArbreRepository;
 import com.example.demo.repository.DetailRecolteRepository;
+import com.example.demo.repository.RecolteRepository;
 import com.example.demo.service.DetailRecolteService;
+import jakarta.servlet.ServletOutputStream;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -24,23 +29,32 @@ public class DetailRecolteServiceImpl implements DetailRecolteService {
     private final DetailRecolteRepository detailRecolteRepository;
     private final ArbreRepository arbreRepository;
 
+    private final RecolteRepository recolteRepository;
+
     private final DetailRecolteMapper detailRecolteMapper;
 
     @Override
     public DetailRecolteResponseDTO addDetailRecolte(DetailRecolteCreateDTO detailRecolteCreateDTO) {
-        UUID arbreId = detailRecolteCreateDTO.getArbre().getId();
-        Arbre arbre = arbreRepository.findById(arbreId)
-                .orElseThrow(() -> new IllegalArgumentException("Arbre not found"));
+        Arbre arbre = arbreRepository.findById(detailRecolteCreateDTO.getArbre().getId())
+                .orElseThrow(() -> new IllegalArgumentException("Invalid arbre: arbre or arbre ID is null"));
 
+        Recolte recolte = recolteRepository.findById(detailRecolteCreateDTO.getRecolte().getId())
+                .orElseThrow(() -> new IllegalArgumentException("Invalid recolte: recolte or recolte ID is null"));
+
+        DetailRecolteId detailRecolteId = new DetailRecolteId(recolte.getId(), arbre.getId());
+
+        DetailRecolte detailRecolte = new DetailRecolte();
         double maxQuantity = calculateMaxQuantity(arbre.getAge());
-
-        DetailRecolte detailRecolte = detailRecolteMapper.createDtotoDetailRecolte(detailRecolteCreateDTO);
         detailRecolte.setQuantiteRecoltee(maxQuantity);
-
+        detailRecolte.setId(detailRecolteId);
+        detailRecolte.setArbre(arbre);
+        detailRecolte.setRecolte(recolte);
+        detailRecolte.setDetailRecolte_id(UUID.randomUUID());
         DetailRecolte savedDetailRecolte = detailRecolteRepository.save(detailRecolte);
 
         return detailRecolteMapper.detailRecolteToResponseDTO(savedDetailRecolte);
     }
+
 
     @Override
     public double calculateMaxQuantity(int age) {
