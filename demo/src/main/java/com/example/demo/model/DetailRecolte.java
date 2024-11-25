547eddf4c329b2a8254199258874fe8fa60ceab1
unknown <ismail.ouali@ump.ac.ma>
2024-11-25 11:37:02 +0100

    CIT-26 Adding another id

--- demo/src/main/java/com/example/demo/model/DetailRecolte.java
+++ demo/src/main/java/com/example/demo/model/DetailRecolte.java
@@ -12,15 +12,18 @@ public class DetailRecolte {
     @EmbeddedId
     private DetailRecolteId id;
 
+    @Column(name = "id", nullable = false, unique = true)
+    private UUID detailRecolte_id; //
+
     private double quantiteRecoltee;
 
     @ManyToOne
     @MapsId("arbreId")
-    @JoinColumn(name = "arbre_id")
+    @JoinColumn(name = "arbre_id", nullable = false)
     private Arbre arbre;
 
     @ManyToOne
     @MapsId("recolteId")
-    @JoinColumn(name = "recolte_id")
+    @JoinColumn(name = "recolte_id", nullable = false)
     private Recolte recolte;
 }
--- demo/src/main/java/com/example/demo/model/DetailRecolteId.java
+++ demo/src/main/java/com/example/demo/model/DetailRecolteId.java
@@ -5,13 +5,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.util.UUID;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Embeddable
-public class DetailRecolteId {
+public class DetailRecolteId implements Serializable {
 
 
     private UUID recolteId;
